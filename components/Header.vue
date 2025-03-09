036d9c5d9923818cb711b70f9208fa21c02c3276
Kazu-Kita <102453289+Kazu-Kita@users.noreply.github.com>
2025-03-09 22:47:11 +0900

    Add header

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,9 @@
 <template>
-    <div class=" fixed h-[calc(100%-6rem)] w-60 top-3 left-3 z-50 rounded-xl shadow-xl">
-        <div class="font-bold text-3xl ">Kazu Kita</div>
+    <div class=" fixed h-[calc(100%-4rem)] w-60 top-2 left-2 z-50 rounded-xl backdrop-blur-xl shadow-xl">
+        <div class="font-bold text-4xl text-center m-4">Kazu Kita</div>
+        <br>
+        <div class="text-2xl text-center">Works</div>
+        <br>
+        <div class="text-2xl text-center">Contact</div>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <Header></Header>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="left-space">
+    <Header/>
+    <div class=" flex text-2xl justify-center">a Hello! I'm Kazu!!</div>
   </div>
 </template>
+<style>
+  .left-space{
+    position: absolute;
+    display: flex;
+    left: 256px;
+  }
+</style>
